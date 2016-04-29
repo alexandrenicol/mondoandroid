bc03998fb7f36888c7ee422ccff3989d87922b75
nicol.alexandre1 <nicol.alexandre1@gmail.com>
2016-04-29 13:41:45 +0100

    started transaction activity

--- app/src/main/java/fr/webnicol/mondoapp/TransactionActivity.java
+++ app/src/main/java/fr/webnicol/mondoapp/TransactionActivity.java
@@ -2,7 +2,12 @@ package fr.webnicol.mondoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TransactionActivity extends AppCompatActivity {
 
@@ -11,6 +16,12 @@ public class TransactionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         Bundle extras = getIntent().getExtras();
         String jsonData;
 
@@ -18,6 +29,45 @@ public class TransactionActivity extends AppCompatActivity {
             jsonData = extras.getString("transaction");
             // and get whatever type user account id is
             Log.d("TRANSACTION", jsonData);
+            try {
+                JSONObject transaction = new JSONObject(jsonData);
+
+                Integer amount = transaction.getInt("amount");
+                String created = transaction.getString("created");
+                String merchantName;
+                String category;
+                String emoji;
+                String imageURL;
+                String addressShortFormatted;
+
+                if (!transaction.isNull("merchant")){
+                    merchantName = transaction.getJSONObject("merchant").getString("name");
+                    category = transaction.getJSONObject("merchant").getString("category");
+                    emoji = transaction.getJSONObject("merchant").getString("emoji");
+                    imageURL = transaction.getJSONObject("merchant").getString("logo");
+
+                    JSONObject merchant = transaction.getJSONObject("merchant");
+
+                    if (!merchant.isNull("address")){
+                        addressShortFormatted = merchant.getJSONObject("address").getString("short_formatted");
+
+                    }
+
+
+                } else {
+                    merchantName = transaction.getString("description");
+                }
+
+
+                TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+                toolbarTitle.setText(created);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
         }
     }
 }
